8b984cdace8ec047dc2005bfb6e0905ddacccee7
Ekta <ekta.gavas96@gmail.com>
2019-11-24 00:58:55 +0530

    Collect more images in NY and store metadata

--- dataset/collect_data.py
+++ dataset/collect_data.py
@@ -8,11 +8,14 @@ EARTH_RADIUS = 6378137
 EQUATOR_CIRCUMFERENCE = 2 * pi * EARTH_RADIUS
 INITIAL_RESOLUTION = EQUATOR_CIRCUMFERENCE / 256.0
 ORIGIN_SHIFT = EQUATOR_CIRCUMFERENCE / 2.0
+
 DIR_PATH = './data/'
 
 # Insert your api key for google maps here
 API_KEY = ''
 
+
+# Function to convert Lat,Long coordinates to pixels
 def latlontopixels(lat, lon, zoom):
     mx = (lon * ORIGIN_SHIFT) / 180.0
     my = log(tan((90 + lat) * pi/360.0))/(pi/180.0)
@@ -22,6 +25,7 @@ def latlontopixels(lat, lon, zoom):
     py = (my + ORIGIN_SHIFT) / res
     return px, py
 
+# Function to convert pixels to Lat,Long coordinates
 def pixelstolatlon(px, py, zoom):
     res = INITIAL_RESOLUTION / (2**zoom)
     mx = px * res - ORIGIN_SHIFT
@@ -31,26 +35,26 @@ def pixelstolatlon(px, py, zoom):
     lon = (mx / ORIGIN_SHIFT) * 180.0
     return lat, lon
 
-############################################
 
-# a neighbourhood in Staten Island, NY:
+# a neighbourhood in Manhatten, NY:
 
-upperleft =  '40.585181,-74.158014'  
-lowerright = '40.574443,-74.140041'
+upperleft =  '40.787322, -73.983632'
+lowerright = '40.743758, -73.855146'
 
 
-zoom = 18   # depending on how many images you want
+zoom = 19   # depending on how many images we want
 
-############################################
 
 ullat, ullon = map(float, upperleft.split(','))
 lrlat, lrlon = map(float, lowerright.split(','))
 
-# Set some important parameters
+# Set parameters
 scale = 1
-maxsize = 500
+maxsize = 600
+bottom = 10
+maxsize -= bottom
 
-# convert all these coordinates to pixels
+# convert latlong coordinates to pixels
 ulx, uly = latlontopixels(ullat, ullon, zoom)
 lrx, lry = latlontopixels(lrlat, lrlon, zoom)
 
@@ -62,32 +66,33 @@ cols, rows = int(ceil(dx/maxsize)), int(ceil(dy/maxsize))
 print(cols, rows)
 
 # calculate pixel dimensions of each small image
-bottom = 120
-largura = int(ceil(dx/cols))
-altura = int(ceil(dy/rows))
-alturaplus = altura + bottom
+width = int(ceil(dx/cols))
+height = int(ceil(dy/rows))
+heightplus = height + bottom
 
-
-final = Image.new("RGB", (int(dx), int(dy)))
+# Store image data
+data = []
 j = 0
 for x in range(cols):
     for y in range(rows):
-        dxn = largura * (0.5 + x)
-        dyn = altura * (0.5 + y)
+        dxn = width * (0.5 + x)
+        dyn = height * (0.5 + y)
         latn, lonn = pixelstolatlon(ulx + dxn, uly - dyn - bottom/2, zoom)
-        position = ','.join((str(latn), str(lonn)))
+        position = ','.join((str(round(latn,6)), str(round(lonn,6))))
         print(x, y, position)
         urlparams = urllib.parse.urlencode({'center': position,
                                       'zoom': str(zoom),
-                                      'size': '%dx%d' % (largura, alturaplus),
+                                      'size': '%dx%d' % (width, heightplus),
                                       'maptype': 'satellite',
                                       'sensor': 'false',
                                       'scale': scale,
-                                      'key': API_KEY})
+                                      'key':API_KEY})
         url = 'http://maps.google.com/maps/api/staticmap?' + urlparams
         f=urllib.request.urlopen(url)
         im=Image.open(BytesIO(f.read()))
         im.save(DIR_PATH+'i'+str(j)+'.png')
+        data.append([round(latn,6), round(lonn,6), str(j)])
         j += 1
-#         final.paste(im, (int(x*largura), int(y*altura)))
-# final.show()
+
+# Create the pandas DataFrame 
+df = pd.DataFrame(data, columns = ['lat', 'lon', 'patch_idx'])
